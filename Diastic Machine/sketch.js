453c89e4d261360f9b3af93406c97d3c2aac2be5
Georgi Dimitrov <lyralei@Georgis-MacBook-Pro.local>
2016-12-17 17:26:16 +0200

    credits to shiffman for this diastic machine example

--- Diastic Machine/sketch.js
+++ Diastic Machine/sketch.js
@@ -1,4 +1,24 @@
+function diastic(seed, words) {
+    var phrase = '';
+    var currentWord = 0;
+
+    for (var i = 0; i < seed.length; i++) {
+        var c = seed.charAt(i);
+
+        for (var j = currentWord; j < words.length; j++) {
+            if (words[j].charAt(i) === c) {
+                phrase += words[j];
+                phrase += ' ';
+                currentWord = j + 1;
+                break;
+            }
+        }
+    }
+    return phrase;
+}
+
 var srctxt;
+var words;
 
 function preload() {
     srctxt = loadStrings('rainbow.txt');
@@ -7,12 +27,13 @@ function preload() {
 function setup() {
     noCanvas();
     srctxt = join(srctxt, ' ');
+    words = splitTokens(srctxt, ' ,!.?');
 
     var seed = select('#seed');
     var submit = select('#submit');
     submit.mousePressed(function() {
-        createP(seed.value());
-        createP(srctxt);
+        var phrase = diastic(seed.value(), words);
+        createP(phrase);
     });
 
 }
